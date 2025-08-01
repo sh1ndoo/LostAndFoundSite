93d30b91f45c5f10d7054e526a3133b26602a5cd
shindoo <trofim-dima06@mail.ru>
2025-08-01 20:48:13 +0700

    add file size limit and check

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from debug_toolbar.toolbar import debug_toolbar_urls
-from django.conf import settings
+from config.settings import STATIC_ROOT
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.sitemaps.views import sitemap
@@ -28,13 +28,14 @@ sitemaps = {
     'ThingSitemap': ThingSitemap,
     'UserSitemap': UserSitemap,
 }
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('apps.things.urls', namespace='things')),
     path('users/', include('apps.users.urls', namespace='users')),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    re_path(r'.txt$', serve, {'document_root': settings.STATIC_ROOT, 'path': "robots.txt"}),
+    re_path(r'^robots\.txt$', serve, {'document_root': STATIC_ROOT, 'path': "robots.txt"}),
 ] + debug_toolbar_urls()
 
 handler404 = 'apps.things.views.view_404'
